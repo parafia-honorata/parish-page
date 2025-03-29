59d9f677b3759c9f5d5a3d8c3adc749336ab6dc2
jlachor <jlachor@syncad.com>
2025-03-29 10:42:00 +0100

    Make adjustement in page style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,30 @@
+"use client"
 import Link from "next/link";
 import Image from "next/image";
 import LogoChurch from "../app/LogoChurch.png";
+import {List} from "lucide-react"
+import useMediaQuery from "@/utils/useMediaQuery";
+import { useState } from "react";
+import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"; 
+import {  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "./ui/select"
+
 
 interface NavbarItem {
   title: string;
   link: string;
   isExternal?: boolean;
+  sublinks?: NavbarItem[];
 }
 
 const Navbar = () => {
+  const isMobile = useMediaQuery("(max-width: 1024px)");
+  const [isDrawerOpened, setIsDrawerOpened] = useState<boolean>(false);
+
   const itemList: NavbarItem[] = [
     {
       title: 'Ogłoszenia',
@@ -24,7 +40,25 @@ const Navbar = () => {
     },
     {
       title: 'Grupy parafialne',
-      link: '/grupy'
+      link: '/grupy',
+      sublinks: [
+        {
+          title: "Koło Biblijne",
+          link: "/kolo-biblijne"
+        },
+        {
+          title: "Żywy Różaniec",
+          link: "/zywy-rozaniec"
+        },
+        {
+          title: "Różaniec mężczyzn",
+          link: "/rozaniec-mezczyzn"
+        },
+        {
+          title: "Salka parafialna",
+          link: "/salka-parafialna"
+        }
+      ]
     },
     {
       title: 'Galeria',
@@ -43,29 +77,61 @@ const Navbar = () => {
   ]
 
   const renderItem = (navbarItem: NavbarItem) => {
+    if (navbarItem.sublinks)
     return (
-      <li className="hover:bg-amber-500 p-4 rounded-sm" key={navbarItem.link}>
+      <Select>
+        <SelectTrigger>{navbarItem.title}</SelectTrigger>
+        <SelectContent>
+          {navbarItem.sublinks.map((sublink) => (
+            <SelectItem key={sublink.link} value={sublink.link}><Link className="hover:bg-amber-500 rounded-sm p-4" href={sublink.link}>{sublink.title}</Link></SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    )
+    return (
+      <li  key={navbarItem.link}>
         { navbarItem.isExternal ?
-            <a href={navbarItem.link} target="_blank" rel="noreferrer" key={navbarItem.link}>{navbarItem.title}</a> 
+            <a className="hover:bg-amber-500 rounded-sm p-4" href={navbarItem.link} target="_blank" rel="noreferrer" key={navbarItem.link}>{navbarItem.title}</a> 
             :
-            <Link href={navbarItem.link}>{navbarItem.title}</Link>
+            <Link className="hover:bg-amber-500 rounded-sm p-4" href={navbarItem.link}>{navbarItem.title}</Link>
         }
       </li>
     )
   }
 
     return (
-      <nav className="fixed top-0 left-0 w-full p-2 shadow-lg bg-amber-800 text-xl">
-        <div className="container flex ">
+      <nav className="font-serif fixed top-0 left-0 w-full p-2 shadow-lg bg-amber-800 text-xl">
+        <div className="container flex justify-between">
           <Link href={"/"}>
-            <Image src={LogoChurch} width={100} height={100} alt="logo" />
+            <Image
+              src={LogoChurch}
+              width={100}
+              height={50}
+              alt="logo"
+              priority={true}
+            />
           </Link>
-          <ul className="flex items-center space-x-4 text-white w-full justify-around">
-            {itemList.map((item) => renderItem(item))}
-          </ul>
+          {isMobile ? (
+            <Sheet >
+              <SheetTrigger><List size={70} color="white"/></SheetTrigger>
+              <SheetContent className="bg-amber-800">
+                <SheetHeader className="pb-4">
+                  <SheetTitle className="text-white">Linki</SheetTitle>
+                </SheetHeader>
+                <ul className="flex items-center flex-col space-x-4 text-white w-full justify-around text-xl gap-4">
+                  {itemList.map((item) => renderItem(item))}
+                </ul>
+              </SheetContent>
+            </Sheet>
+            
+          ) : (
+            <ul className="flex items-center space-x-4 text-white w-full justify-around">
+              {itemList.map((item) => renderItem(item))}
+            </ul>
+          )}
         </div>
       </nav>
-  )
+    );
 
 }
 
---
+++ utils/useMediaQuery.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(query);
+    setMatches(mediaQuery.matches);
+
+    const handleChange = () => setMatches(mediaQuery.matches);
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, [query]);
+
+  return matches;
+};
+
+export default useMediaQuery;
